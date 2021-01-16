f4f5305c792e5d5b38cb54474f2cb89eec5d537a
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-16 16:32:57 -0500

    commit tree ffd35e2049bda1a2464b49e7286c43ed23c01b36

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+i = 0
 def key_for_min_value(name_hash)
-
+    lowest_val = 0
+    lowest_key = nil
+    name_hash.each do |key, value|
+        if lowest_val == 0 || value < lowest_val
+            lowest_key = key
+            lowest_val = value
+        end
+    end
+    lowest_key
 end
+
+
+
